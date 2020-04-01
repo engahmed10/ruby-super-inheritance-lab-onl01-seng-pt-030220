8f45a48b3d0450fc48e52d69f54c482a0d4601bd
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-01 09:05:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
    def hello
-      phrase = "Hey there! I'm so excited to learn stuff.\n"
+      "Hey there! I'm so excited to learn stuff.\n"
    end
 
    def raise_hand
-     phrase = 'Pick me!'
+     'Pick me!'
    end
 
 end
